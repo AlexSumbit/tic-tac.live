93515b4fcc08be67f91b92a32e0e002862fbbe00
AlexSumbit <alexsumbit@gmail.com>
2018-08-15 00:18:28 +0300

    base socket connection

--- server/server.js
+++ server/server.js
@@ -13,17 +13,55 @@ let clientCount = 0;
 let queue = "";
 
 
+function message(name, data) {
+    return {
+        name: name,
+        data: data
+    }
+}
+
+
+function sendClientCount() {
+    io.emit("message", message("client-count", clientCount));
+}
+
+function startGame(socket) {
+    if(queue != "") {
+        socket.join(queue);
+        console.log("Join room: ", queue);
+        io.in(queue).emit("message", message("game-started", {}));
+        queue = "";
+    } else {
+        queue = socket.id;
+        socket.join(queue);
+        console.log("Create room: ", queue);
+    }
+}
+
+
 server.listen(config.port, () => {
     console.log('Server listening at port %d', config.port);
 });
 
 io.on("connection", (socket) => {
-    console.info(socket.id, " Is Connected");
     clientCount ++;
+    sendClientCount();
 
-    socket.on("disconnect", () => {
-        console.info(socket.id, " Has Disconnected");
+    socket.on("message", (event) => {
 
+        switch (event.name) {
+            case "start-game": startGame(socket); break;
+            case "turn": break;
+        }
+    });
+    
+
+    socket.on("disconnecting", (reason) => {
         clientCount --;
+        sendClientCount();
+
+        for(let room in socket.rooms) {
+            io.to(room).emit("message", message("disconnected", {}));
+        }
     });
 });
